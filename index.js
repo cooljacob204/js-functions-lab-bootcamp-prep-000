33dd441c204f89e50b3df329366b8a3f09fe04cb
Jacob Feldman <jacobgfeld@gmail.com>
2018-09-15 20:53:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 function happyHolidays(){return "Happy Holidays!"}
 
-function happyHolidaysTo(name){return `Happy Holidays, ${name}!`
+function happyHolidaysTo(name){return `Happy Holidays, ${name}!`} 
 
-function happyHolidaysTo(holiday, name){return `Happy ${holiday}, ${name}!`
+function happyHolidaysTo(holiday, name){return `Happy ${holiday}, ${name}!`}
 
-function holidayCountdown(holiday, days){return `It's ${days} days until ${holiday}!`
+function holidayCountdown(holiday, days){return `It's ${days} days until ${holiday}!`}
